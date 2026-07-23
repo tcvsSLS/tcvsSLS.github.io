a663d3d211ca28bf803777665e209d9de11e75a6
tcvsSLS <109934461+tcvsSLS@users.noreply.github.com>
2026-07-23 14:11:49 +0800

    Refactor API URLs in config.js

--- course-retake/js/config.js
+++ course-retake/js/config.js
@@ -1,2 +1,6 @@
-// 全域設定檔
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzKUkKyGcfSKyJyAQhh8dGUx9005y4OZ4lV8SUxw2nGUQvaL9jT2RDdOIXMYY3RprlG/exec";
+// 🌟 專案全域 API 設定檔
+const ADMIN_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzKUkKyGcfSKyJyAQhh8dGUx9005y4OZ4lV8SUxw2nGUQvaL9jT2RDdOIXMYY3RprlG/exec";
+const STUDENT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbz1viSsMr-yLhx146zzonHCKl4Qdp-YD6c98GcZpTqgh_6zpSixejNuQHnZMfmMq6R0Sw/exec";
+
+// 為了相容管理端的 api.js，將預設 GAS_WEB_APP_URL 指向管理端
+const GAS_WEB_APP_URL = ADMIN_WEB_APP_URL;
